3f9ef4d498f0285a6873db5b5f699632b4a263eb
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-19 20:07:48 +0530

    Middleware , auth Route

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const { AsyncHandler } = require("./utils/async-handler");
+const authRoute = require("./routes/auth/auth.js");
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 8000;
 
+app.use("/auth", authRoute);
+
 app.get("/",AsyncHandler(async (req,res)=>{
-    
-}))
+    res.send("YO !!👋❤")
+}));
 
 app.listen(PORT,()=>{console.log(`Server started at ${PORT}`)});
