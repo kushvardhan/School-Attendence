14976c839a57f2958b8616daf90767105c4e3bb6
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-19 20:16:53 +0530

    corrected Name , converted TO CommonJs convention

--- utils/async-handler.js
+++ utils/async-handler.js
@@ -1,12 +1,13 @@
-export function AsyncHandler(fn){
-    return async function (req,res,next){
-        try{
-            const result = await fn(req,res,next);
-            return result;
-        }catch(error){
-            return res.status(500).json({
-                error:'Internal Server Error'
-            });
-        }
+const AsyncTryCatchHandler = (fn) => {
+  return async function (req, res, next) {
+    try {
+      return await fn(req, res, next);
+    } catch (error) {
+      return res.status(500).json({
+        error: "Internal Server Error",
+      });
     }
-}
+  };
+};
+
+module.exports = { AsyncTryCatchHandler };
