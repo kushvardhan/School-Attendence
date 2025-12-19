6bc5fa29f2ca20f46792faaf7c7e807ba6b91407
Kush Vardhan <kushvardhan39797@gmail.com>
2025-12-19 19:40:30 +0530

    Server port

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 8000;
 
 app.get("/",(req,res)=>{
     res.send("YO YE");
 })
 
-app.listen("8000",()=>{console.log("Server started")});
+app.listen(PORT,()=>{console.log(`Server started at ${PORT}`)});
